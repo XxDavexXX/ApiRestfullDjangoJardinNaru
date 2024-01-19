1cf966351529e89c606ae3a6d41cf42daab6fb3f
XxDavexX <dave.santivanez@gmail.com>
2024-01-19 15:41:24 -0500

    termino de los filtros

--- api/serializers.py
+++ api/serializers.py
@@ -24,8 +24,18 @@ class PlantaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Planta
         fields = ['id', 'nombre', 'descripcion', 'precio', 'stock', 'categoria', 'imgplanta', 'tipo_planta_id']
-        
 
+    def update(self, instance, validated_data):
+        tipo_planta_data = validated_data.pop('tipo_planta_id', None)
+        
+        # Lógica de actualización para el campo anidado 'tipo_planta_id'
+        if tipo_planta_data:
+            tipo_planta_instance = instance.tipo_planta_id
+            TipoPlantaSerializer().update(tipo_planta_instance, tipo_planta_data)
+
+        # Resto de la lógica de actualización para otros campos
+        return super().update(instance, validated_data)
+    
 class CarritoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Carrito
